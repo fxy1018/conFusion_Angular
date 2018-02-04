42b6da5188c1b1b8ba1979c3b18e6b522899dc9f
fxy1018 <xueyi.fan27@gmail.com>
2018-02-04 00:04:23 -0500

    update leader

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private leaderService: LeaderService) { }
 
+  leaders: Leader[]
   ngOnInit() {
+    this.leaders = this.leaderService.getLeaders();
   }
 
 }
