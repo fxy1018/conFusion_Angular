2e7ec9f496dfc8b4d83037561b033224ecd6db56
fxy1018 <xueyi.fan27@gmail.com>
2018-02-03 22:10:04 -0500

    route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,21 @@ import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule} from '@angular/flex-layout';
 
+
 import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { DishService} from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService} from './services/dish.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +29,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -30,9 +40,10 @@ import { FooterComponent } from './footer/footer.component';
     HttpModule,
     MaterialModule,
     FlexLayoutModule,
+    AppRoutingModule
 
   ],
-  providers: [ DishService,],
+  providers: [ DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
