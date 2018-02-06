15b913a217662e8ade37134e6d4d04c1849792bf
fxy1018 <xueyi.fan27@gmail.com>
2018-02-05 23:25:15 -0500

    angular form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,12 +17,14 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 import { DishService} from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 
 @NgModule({
@@ -34,7 +36,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -42,8 +45,12 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     HttpModule,
     MaterialModule,
     FlexLayoutModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
 
+  ],
+  entryComponents: [
+        LoginComponent
   ],
   providers: [ DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
