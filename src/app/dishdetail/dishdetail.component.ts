e5bd1cb68d130b555dba06fe0004308f12a114cb
fxy1018 <xueyi.fan27@gmail.com>
2018-03-18 20:27:04 -0400

    rest save

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -16,6 +16,7 @@ import 'rxjs/add/operator/switchMap';
 })
 export class DishdetailComponent implements OnInit {
   dish: Dish;
+  dishcopy = null;
   dishIds: number[];
   prev: number;
   next: number;
@@ -54,7 +55,7 @@ export class DishdetailComponent implements OnInit {
 
     this.route.params
     .switchMap((params: Params)=> this.dishService.getDish(+params['id']))
-    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id),
+    .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id),
       errmess => this.errMess = <any>errmess});
   }
 
@@ -102,10 +103,11 @@ export class DishdetailComponent implements OnInit {
     this.comment = this.commentForm.value;
     const date = new Date();
     this.comment.date = date.toISOString();
-    this.dish.comments.push(this.comment);
-
-
     console.log(this.comment);
+
+    this.dishcopy.comments.push(this.comment);
+    this.dishcopy.save()
+      .subscribe(dish => this.dish = dish);
     this.commentForm.reset({
       author: '',
       comment: '',
